a2c6640a0ac09c756f58c4d0159b850c3a70da6f
Wurdsmith <wurdsmithing@gmail.com>
2021-01-20 20:12:14 -0600

    commit tree 83f49057ad81dc10e5f5c9d6ce4dd3c23e44a1c2

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+class School
+    
+attr_accessor :school_name, :roster
+
+    def initialize(school_name)
+        @school_name = school_name
+        @roster = {}
+    end
+
+    def add_student(name, grade)
+        if @roster[grade] != nil
+        @roster[grade] << name
+        else
+        @roster[grade] = [name]
+        end
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end 
+
+    def sort 
+        roster.each do |x, y| 
+          y.sort!
+        end 
+    end 
+end
+
